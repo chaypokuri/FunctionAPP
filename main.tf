75832b0241a0146369dd30e34b616110a1c97dd1
chaypokuri <61907472+chaypokuri@users.noreply.github.com>
2024-08-22 19:57:19 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,70 @@
+provider "azurerm" {
+  features {}
+  subscription_id = "c2bd123a-183f-43d5-bf41-c725494e595a"
+  tenant_id       = "3180c264-31bc-4113-8f50-b7393a40457b"
+  client_id       = "1a046c02-8c39-4f1d-b30b-93f41a9c6b15"
+  client_secret   = "kUz8Q~qwom0J-MM5ZNqexXyUOguygMj5QELdhdl5"
+}
+ 
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+}
+ 
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "West Europe"
+}
+ 
+resource "azurerm_app_service_plan" "example" {
+  name                = "example-appserviceplan"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  os_type             = "Linux"
+  sku_name            = "Y1"
+}
+ 
+resource "azurerm_virtual_network" "example" {
+  name                = "example-virtual-network"
+  address_space       = ["10.0.0.0/24"]
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+}
+ 
+resource "azurerm_subnet" "example" {
+  name                 = "example-subnet"
+  resource_group_name  = azurerm_resource_group.example.name
+  virtual_network_name = azurerm_virtual_network.example.name
+  address_prefixes     = ["10.0.1.0/27"]
+ 
+  delegation {
+    name = "example-delegation"
+ 
+    service_delegation {
+      name    = "Microsoft.Web/serverFarms"
+      actions = ["Microsoft.Network/virtualNetworks/subnets/action"]
+    }
+  }
+}
+ 
+
+resource "azurerm_linux_function_app" "example" {
+  name                = "example-linux-function-app"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  service_plan_id            = azurerm_service_plan.example.id
+
+  site_config 
+    {
+  vnet_route_all_enabled = true
+    }
+}
+ 
+resource "azurerm_app_service_virtual_network_swift_connection" "example" {
+  app_service_id = azurerm_app_service.example.id
+  subnet_id      = azurerm_subnet.example.id
+}

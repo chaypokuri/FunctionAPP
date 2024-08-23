d044664ba39528230408caec0b62a667026cd2d0
chaypokuri <61907472+chaypokuri@users.noreply.github.com>
2024-08-23 18:38:25 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -43,10 +43,20 @@ resource "azurerm_subnet" "example" {
     }
   }
 }
+resource "azurerm_storage_account" "example" {
+  name                     = "linuxfunctionappsa"
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
 resource "azurerm_linux_function_app" "example" {
   name                = "example-linux-function-app"
   resource_group_name = azurerm_resource_group.example.name
   location            = azurerm_resource_group.example.location
+  storage_account_name       = azurerm_storage_account.example.name
+  storage_account_access_key = azurerm_storage_account.example.primary_access_key
   service_plan_id     = azurerm_service_plan.example.id
  
   site_config {

2b428745e978a83c1b074b1bae148cb0cabd99cd
chaypokuri <61907472+chaypokuri@users.noreply.github.com>
2024-09-13 16:57:12 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 provider "azurerm" {
   features {}
-  subscription_id = "c2bd123a-183f-43d5-bf41-c725494e595a"
-  tenant_id = "3180c264-31bc-4113-8f50-b7393a40457b"
-  client_id = "1a046c02-8c39-4f1d-b30b-93f41a9c6b15"
-  client_secret = "kUz8Q~qwom0J-MM5ZNqexXyUOguygMj5QELdhdl5"
+subscription_id = "c2bd123a-183f-43d5-bf41-c725494e595a"
+tenant_id = "3180c264-31bc-4113-8f50-b7393a40457b"
+client_id = "1a046c02-8c39-4f1d-b30b-93f41a9c6b15"
+client_secret = "kUz8Q~qwom0J-MM5ZNqexXyUOguygMj5QELdhdl5"
 }
 
 resource "azurerm_resource_group" "example" {
@@ -37,22 +37,4 @@ resource "azurerm_linux_function_app" "example" {
   service_plan_id            = azurerm_service_plan.example.id
 
   site_config {}
-}
-
-resource "azurerm_logic_app_standard" "example" {
-  name                     = "example-logic-app"
-  resource_group_name      = azurerm_resource_group.example.name
-  location                 = azurerm_resource_group.example.location
-  storage_account_name     = azurerm_storage_account.example.name
-  storage_account_access_key = azurerm_storage_account.example.primary_access_key
-  app_service_plan_id      = azurerm_service_plan.example.id
-
-  identity {
-    type = "SystemAssigned"
-    sku_name = "Standard" 
-  }
-  depends_on = [
-    azurerm_service_plan.example,
-    azurerm_storage_account.example
-  ]
 }

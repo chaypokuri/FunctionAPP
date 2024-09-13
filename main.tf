13d680a4ec90ce2d13f686567823e1179605836e
chaypokuri <61907472+chaypokuri@users.noreply.github.com>
2024-09-13 18:17:44 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,5 @@
 provider "azurerm" {
   features {}
-  subscription_id = "c2bd123a-183f-43d5-bf41-c725494e595a"
-  tenant_id       = "3180c264-31bc-4113-8f50-b7393a40457b"
-  client_id       = "1a046c02-8c39-4f1d-b30b-93f41a9c6b15"
-  client_secret   = "kUz8Q~qwom0J-MM5ZNqexXyUOguygMj5QELdhdl5"
 }
 
 resource "azurerm_resource_group" "example" {
@@ -12,31 +8,21 @@ resource "azurerm_resource_group" "example" {
 }
 
 resource "azurerm_storage_account" "example" {
-  # Invalid name format: too short
-  name                     = "a1b"  # Must be between 3 and 24 characters, only lowercase letters and numbers
+  name                     = "linuxfunctionappsa"
   resource_group_name      = azurerm_resource_group.example.name
   location                 = azurerm_resource_group.example.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
 }
 
-resource "azurerm_service_plan" "example" {
-  name                = "example-app-service-plan"
-  resource_group_name = azurerm_resource_group.example.name
-  location            = azurerm_resource_group.example.location
-  os_type             = "Linux"
-  sku_name            = "Y1"
-}
-
 resource "azurerm_linux_function_app" "example" {
   name                = "example-linux-function-app"
-  resource_group_name = azurerm_resource_group.example.name
-  location            = azurerm_resource_group.example.location
+  resource_group_name = "example-testrg"
+  location            = "East-us"
 
-  # Invalid values to simulate failures
-  storage_account_name       = "validname"  # Valid name but invalid key
-  storage_account_access_key = "invalidkey!@#"  # Invalid key format
-  service_plan_id            = "/subscriptions/12345678-1234-5678-1234-123456789012/resourceGroups/invalid-group/providers/Microsoft.Web/serverFarms/invalid-service-plan"  # Incorrect format
-
+  storage_account_name       = "teststorageaccount001"
+  storage_account_access_key = "BjYCZEHX2OCqz/HWJdWp4rHfpLIAMJt0adLxzQnpItXCSyKwArG8iCgFfVNW/6pgabrtlniTcEaK+AStF5N8xQ=="
+  service_plan_id            = "/subscriptions/c2bd123a-183f-43d5-bf41-c725494e595a/resourceGroups/test-rg/providers/Microsoft.Web/serverfarms/test-asp"
+  virtual_network_subnet_id = "/subscriptions/c2bd123a-183f-43d5-bf41-c725494e595a/resourceGroups/test-rg/providers/Microsoft.Network/virtualNetworks/test-rg/subnets/test-subnet"
   site_config {}
 }

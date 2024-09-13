f0254402533b8514ea71a0312da4c2645ba5268a
chaypokuri <61907472+chaypokuri@users.noreply.github.com>
2024-09-13 17:37:03 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,7 +13,7 @@ resource "azurerm_resource_group" "example" {
 
 resource "azurerm_storage_account" "example" {
   # Invalid due to invalid characters and length
-  name                     = "inv@lidname!"  # Invalid name (contains special characters)
+  name                     = "linuxfunctionappsa"  # Invalid name (contains special characters)
   resource_group_name      = azurerm_resource_group.example.name
   location                 = azurerm_resource_group.example.location
   account_tier             = "Standard"

da00260b3f965f178a8efad3670c1af402e2b122
chaypokuri <61907472+chaypokuri@users.noreply.github.com>
2024-09-19 16:13:03 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -52,7 +52,7 @@ resource "azurerm_app_service_plan" "this" {
 }
 
   resource "azurerm_function_app_standard" "this" {
-  name                       = "test-azure-Fapp"
+  name                       = "test-azure-fapp"
   location                   = azurerm_resource_group.this.location
   resource_group_name        = azurerm_resource_group.this.name
   app_service_plan_id        = azurerm_app_service_plan.this.id

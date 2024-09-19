68391965d67942f3a8acb1f05254da7db4800e55
chaypokuri <61907472+chaypokuri@users.noreply.github.com>
2024-09-19 16:01:29 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,33 +1,61 @@
 provider "azurerm" {
   features {}
-subscription_id = "c2bd123a-183f-43d5-bf41-c725494e595a"
-tenant_id = "3180c264-31bc-4113-8f50-b7393a40457b"
-client_id = "1a046c02-8c39-4f1d-b30b-93f41a9c6b15"
-client_secret = "kUz8Q~qwom0J-MM5ZNqexXyUOguygMj5QELdhdl5"
-
+}
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+}
+resource "azurerm_resource_group" "this" {
+  name     = "azure-test-rg"
+  location = "East US"
 }
 
-resource "azurerm_resource_group" "example" {
-  name     = "example-resources"
-  location = "West Europe"
-}
-
-resource "azurerm_storage_account" "example" {
-  name                     = "linuxfunctionappsa"
-  resource_group_name      = azurerm_resource_group.example.name
-  location                 = azurerm_resource_group.example.location
+resource "azurerm_storage_account" "this" {
+  name                     = "Functionapptestsa"
+  resource_group_name      = azurerm_resource_group.this.name
+  location                 = azurerm_resource_group.this.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
 }
+resource "azurerm_virtual_network" "this" {
+  name                = "this-virtual-network"
+  address_space       = ["10.0.0.0/24"]
+  location            = azurerm_resource_group.this.location
+  resource_group_name = azurerm_resource_group.this.name
+}
+resource "azurerm_subnet" "this" {
+  name                 = "this-subnet"
+  resource_group_name  = azurerm_resource_group.this.name
+  virtual_network_name = azurerm_virtual_network.this.name
+  address_prefixes     = ["10.0.1.0/27"]
+  delegation {
+    name = "this-delegation"
+    service_delegation {
+      name    = "Microsoft.Web/serverFarms"
+      actions = ["Microsoft.Network/virtualNetworks/subnets/action"]
+    }
+  }
+}
+resource "azurerm_linux_function_app" "this" {
+  name                = "azure-functions-test-service-plan"
+  resource_group_name = "azurerm_resource_group.this.name"
+  location            = "azurerm_resource_group.this.location"
+  kind                = "elastic"
+  sku {
+    tier = "WorkflowStandard"
+    size = "WS1"
+  }
+}
 
-resource "azurerm_linux_function_app" "example" {
-  name                = "example-linux-function-app"
-  resource_group_name = "example-testrg"
-  location            = "East-us"
-
-  storage_account_name       = "teststorageaccount001"
-  storage_account_access_key = "BjYCZEHX2OCqz/HWJdWp4rHfpLIAMJt0adLxzQnpItXCSyKwArG8iCgFfVNW/6pgabrtlniTcEaK+AStF5N8xQ=="
-  service_plan_id            = "/subscriptions/c2bd123a-183f-43d5-bf41-c725494e595a/resourceGroups/test-rg/providers/Microsoft.Web/serverfarms/test-asp"
-  virtual_network_subnet_id = "/subscriptions/c2bd123a-183f-43d5-bf41-c725494e595a/resourceGroups/test-rg/providers/Microsoft.Network/virtualNetworks/test-rg/subnets/test-subnet"
-  site_config {}
+  resource "azurerm_Function_app_standard" "this" {
+  name                       = "test-azure-Fapp"
+  location                   = azurerm_resource_group.this.location
+  resource_group_name        = azurerm_resource_group.this.name
+  app_service_plan_id        = azurerm_app_service_plan.this.id
+  storage_account_name       = azurerm_storage_account.this.name
+  storage_account_access_key = azurerm_storage_account.this.primary_access_key
 }

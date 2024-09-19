b59253ae7c4bed266e7422d6d3c2180c0c976f13
chaypokuri <61907472+chaypokuri@users.noreply.github.com>
2024-09-19 16:16:37 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -58,5 +58,5 @@ resource "azurerm_app_service_plan" "this" {
   app_service_plan_id        = azurerm_app_service_plan.this.id
   storage_account_name       = azurerm_storage_account.this.name
   storage_account_access_key = azurerm_storage_account.this.primary_access_key
-  os_type                    = "Linux"
+  
 }

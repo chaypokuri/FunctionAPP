10e57c513921ec979f95591cf855184ba656dcfb
chaypokuri <61907472+chaypokuri@users.noreply.github.com>
2024-09-19 16:41:08 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,7 @@
 provider "azurerm" {
   features {}
 }
+
 terraform {
   required_providers {
     azurerm = {
@@ -9,6 +10,7 @@ terraform {
     }
   }
 }
+
 resource "azurerm_resource_group" "this" {
   name     = "azure-test-rg"
   location = "East US"
@@ -21,17 +23,20 @@ resource "azurerm_storage_account" "this" {
   account_tier             = "Standard"
   account_replication_type = "LRS"
 }
+
 resource "azurerm_virtual_network" "this" {
   name                = "this-virtual-network"
   address_space       = ["10.0.0.0/24"]
   location            = azurerm_resource_group.this.location
   resource_group_name = azurerm_resource_group.this.name
 }
+
 resource "azurerm_subnet" "this" {
   name                 = "this-subnet"
   resource_group_name  = azurerm_resource_group.this.name
   virtual_network_name = azurerm_virtual_network.this.name
   address_prefixes     = ["10.0.1.0/27"]
+
   delegation {
     name = "this-delegation"
     service_delegation {
@@ -40,23 +45,44 @@ resource "azurerm_subnet" "this" {
     }
   }
 }
+
 resource "azurerm_app_service_plan" "this" {
   name                = "azure-functions-test-service-plan"
-  resource_group_name = "azurerm_resource_group.this.name"
-  location            = "azurerm_resource_group.this.location"
-  kind                = "elastic"
+  resource_group_name = azurerm_resource_group.this.name
+  location            = azurerm_resource_group.this.location
+  os_type             = "Linux"
   sku {
-    tier = "WorkflowStandard"
-    size = "WS1"
+    tier     = "WorkflowStandard"
+    size     = "WS1"
+    capacity = 1
   }
 }
 
-  resource "azurerm_linux_function_app" "this" {
+resource "azurerm_linux_function_app" "this" {
   name                       = "test-azure-fapp"
   location                   = azurerm_resource_group.this.location
   resource_group_name        = azurerm_resource_group.this.name
   service_plan_id            = azurerm_app_service_plan.this.id
   storage_account_name       = azurerm_storage_account.this.name
   storage_account_access_key = azurerm_storage_account.this.primary_access_key
-  
+
+  site_config {
+    application_stack {
+      linux_fx_version = "DOCKER|mcr.microsoft.com/azure-functions/dotnet:3.0"
+    }
+  }
+
+  app_settings = {
+    "WEBSITE_RUN_FROM_PACKAGE" = "1"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  virtual_network_subnet_id = azurerm_subnet.this.id
+}
+
+output "function_app_default_hostname" {
+  value = azurerm_linux_function_app.this.default_hostname
 }

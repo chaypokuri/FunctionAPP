da097e3a1b942e32a2e6e8abaac81de723481b4f
chaypokuri <61907472+chaypokuri@users.noreply.github.com>
2024-09-19 16:53:28 +0530

    Update main.tf for FA 3

--- main.tf
+++ main.tf
@@ -61,7 +61,7 @@ resource "azurerm_linux_function_app" "this" {
   name                       = "test-azure-fapp"
   location                   = azurerm_resource_group.this.location
   resource_group_name        = azurerm_resource_group.this.name
-  service_plan_id            = azurerm_app_service_plan.this.id
+  service_plan_id            = azurerm_service_plan.this.id
   storage_account_name       = azurerm_storage_account.this.name
   storage_account_access_key = azurerm_storage_account.this.primary_access_key
   virtual_network_subnet_id = azurerm_subnet.this.id
